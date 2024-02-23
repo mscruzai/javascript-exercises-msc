13adeaf7bfa97cfb8e9290afcc44eff724958976
mscruzai <152240662+mscruzai@users.noreply.github.com>
2024-02-23 10:40:08 +0800

    Finished repeat string exercise.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, repeats) {
+    
+    if(repeats < 0)
+        return "ERROR";
+    let repeatedString = "";
 
+    while (repeats > 0){
+        repeatedString += string;
+        repeats--;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
