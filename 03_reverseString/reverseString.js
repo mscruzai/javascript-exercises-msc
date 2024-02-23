f40816e58bbe0acc0dc3aad5477eafd7bc82a38d
mscruzai <152240662+mscruzai@users.noreply.github.com>
2024-02-23 12:09:34 +0800

    Finished reversed string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    
+    let strLength = string.length;
+    let strArray =  string.split("");
+    const reversedArray =[];
 
+    for(i=0 ; i < strLength ; i++){
+        reversedArray[i] = strArray[strLength - 1 - i];
+    } 
+    return reversedArray.join("");
 };
 
 // Do not edit below this line
