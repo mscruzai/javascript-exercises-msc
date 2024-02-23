cd713300b793bedff0d32016a4d7b557f992ca6c
mscruzai <152240662+mscruzai@users.noreply.github.com>
2024-02-23 14:07:31 +0800

    Finished sumAll.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(naturalA, naturalB) {
+    
+    let sum=0;
 
+    if(!Number.isInteger(naturalA) || !Number.isInteger(naturalB) )
+        return "ERROR";
+    if( naturalA < 0 || naturalB < 0 )
+        return "ERROR";
+    if (naturalA > naturalB){
+        let temp = naturalB;
+        naturalB = naturalA;
+        naturalA = temp;
+    }
+
+    for(i = naturalA; i <= naturalB ; sum+=i, i++);
+    return sum;
 };
 
 // Do not edit below this line
