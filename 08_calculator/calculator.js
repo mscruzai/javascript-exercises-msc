a945160f50674e26c2b858d0e0d9a65189545e29
mscruzai <152240662+mscruzai@users.noreply.github.com>
2024-03-19 16:58:19 +0800

    Implemented factorial using arrays and higher order functions. Finished calculator exercise.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,20 @@
-const add = function() {
-	
-};
+const add = (a,b) => (a + b);
 
-const subtract = function() {
-	
-};
+const subtract = (a, b) => (a-b);
 
-const sum = function() {
-	
-};
+const sum = (arr) => arr.reduce((total, currentItem) => total + currentItem, 0);
 
-const multiply = function() {
+const multiply = (arr) => arr.reduce((product, currentItem) => product*currentItem, 1);
 
-};
+const power =  (a, b) => Math.pow(a,b);
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
-
+const factorial = (n) =>
+  (n > 1) 
+    ? Array.from({ length: n }, (_, i) => i + 1).reduce((acc, val) => acc * val, 1)  
+    : n < 0
+      ? undefined
+      : 1;
+      
 // Do not edit below this line
 module.exports = {
   add,
