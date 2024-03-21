4f0c6a86f535c00f4034696459703f217d3a5c1a
mscruzai <152240662+mscruzai@users.noreply.github.com>
2024-03-21 16:40:14 +0800

    Implemented zeroth member, string inputs, negative inputs and the spec.js tests.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,10 +1,13 @@
-const fibonacci = function(n) {
-    
-    // initial members
-    a = 0;
-    b = 1;
+const fibonacci = function(nth) {
 
-    for(i = 1; i < n ; i++){
+    n = (typeof nth !== 'number'
+        ? parseInt(nth)
+        : nth) ;
+    
+    if(n < 0) return "OOPS";
+    if(n == 0) return 0;
+
+    for(a = 0, b = 1, i = 1; i < n ; i++){
         c =  b;
         b = a + b;
         a = c;
