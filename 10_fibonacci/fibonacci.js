9e97b1673ac40066353fa2fad63fcf6f898cd234
mscruzai <152240662+mscruzai@users.noreply.github.com>
2024-03-21 16:25:11 +0800

    Wrote initial code. First try.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    
+    // initial members
+    a = 0;
+    b = 1;
+
+    for(i = 1; i < n ; i++){
+        c =  b;
+        b = a + b;
+        a = c;
+    }
+    return b;
 
 };
 
