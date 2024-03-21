139d04eba0af5e8e13efab0cf206b4f6498f143b
mscruzai <152240662+mscruzai@users.noreply.github.com>
2024-03-21 15:30:21 +0800

    Finished palindromes exercise using charAt looping.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.replace(/\W/ig, "").toUpperCase();
+    s = str.length;
+    for(i=0; i < s ; i++){
+        if(str.charAt(i) !== str.charAt(s-1-i)){
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
