a633033a247278e6c827af954d06544286f01a19
mscruzai <152240662+mscruzai@users.noreply.github.com>
2024-03-22 12:48:04 +0800

    Wrote getOldest function using reduce.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(arrPeople) {
+    return arrPeople.reduce(
+        (oldest, person ) => {
+            
+            let personAge = !person.yearOfDeath
+                  ? (new Date().getFullYear() - person.yearOfBirth)
+                  : (person.yearOfDeath - person.yearOfBirth);
 
+            if( personAge > (oldest.yearOfDeath - oldest.yearOfBirth)){
+                oldest = person;
+            }
+            
+            return oldest;
+        }
+    );
 };
 
 // Do not edit below this line
